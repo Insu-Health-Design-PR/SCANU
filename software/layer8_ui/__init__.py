0ab131fc8305bb24fda2c0121828f721c88c96dc
Adrianinsu <adrian@insuhealthdesign.com>
2026-03-20 10:47:23 -0400

    Delete software/layer8_ui/__init__.py

--- software/layer8_ui/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Public API for Layer 8 UI/backend stream."""
-
-from .backend_api import BackendAPI, SystemStatus
-from .websocket_stream import WebSocketStream
-
-__all__ = ["BackendAPI", "SystemStatus", "WebSocketStream"]
